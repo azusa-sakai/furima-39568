f53ea3fd5ee05b51184f0c7578159e834744d449
azusa-sakai <azusa.sakai.i@gmail.com>
2023-12-06 20:51:28 +0900

    テーブル設計、itemsコントローラー定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    @items = Items.all
   end
 
   def new
+    @item = Items.new
   end
-  
+
+  def create
+    Items.create(item_params)
+    redirect_to '/'
+  end
+
 end
