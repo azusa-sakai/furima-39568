5c01b646fefebf63848cb2a7cbe294b8d2159002
azusa-sakai <azusa.sakai.i@gmail.com>
2023-12-07 08:37:07 +0900

    販売価格バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
+                presence: {message: "can't be blank" }
 end
