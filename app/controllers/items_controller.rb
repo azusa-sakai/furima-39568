33c244656b348e074a8659ead276180a399b0464
azusa-sakai <azusa.sakai.i@gmail.com>
2023-12-20 17:02:01 +0900

    ページ下部の「もどる」ボタン遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(items_params)
+    redirect_to root_path
+  end
+
 
   private
 
