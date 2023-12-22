5c3e7e9c8a38d9ce846221e56a11c6ea7c30676e
azusa-sakai <azusa.sakai.i@gmail.com>
2023-12-22 20:33:39 +0900

    private内にまとめてbefore_actionで呼び出す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,19 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user != @item.user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
-
       if @item.update(items_params)
         redirect_to @item
       else
@@ -41,6 +38,9 @@ class ItemsController < ApplicationController
 
 
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def items_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :days_until_shipping_id, :price, :image).merge(user_id: current_user.id)
